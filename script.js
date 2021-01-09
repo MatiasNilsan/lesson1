697156d5bb794c9dd1c1b511353bab4e58637247
MatiasNilsan <matiasnilsan@gmail.com>
2021-01-09 23:07:25 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+const text = fs.readFileSync('script.js', 'utf-8');
+
+console.log(text);
---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const body = req.url === '/styles.css'
+        ? fs.readFileSync('./public/styles.css')
+        : fs.readFileSync('./public/index.html');
+    res.end(body);
+})
+
+server.listen(3000);
+
+console.log('Server started');
